6d744e59c319ae41071f83dfcf7c64dac023e855
Sebastian-ibanezV <80800429+Sebastian-ibanezV@users.noreply.github.com>
2026-03-02 01:26:25 -0400

    Update lab1.py
    documentation

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -2,43 +2,42 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 1) Cargar imagen en escala de grises (0..255)
-img = np.array(Image.open("imagen.jpg").convert("L"), dtype=np.uint8)
-
+img = np.array(Image.open("imagen.jpg").convert("L"), dtype=np.uint8) #cargar imagen
+#parametros
 L = 256
 N = img.size
 
-# 2) Histograma n_k
+#histograma n_k
 hist = np.bincount(img.ravel(), minlength=L).astype(np.float64)
 
-# 3) PDF y CDF
+#PDF CDF
 pdf = hist / N
 cdf = np.cumsum(pdf)
 
-# 4) Ecualización (manual): s_k = (L-1)*CDF(k)
+#ecualizacion
 lut = np.round((L - 1) * cdf).astype(np.uint8)
 eq = lut[img]
 
-# 5) Mostrar resultados (2x2)
+#show
 x = np.arange(L)
 
 plt.figure(figsize=(12, 7))
 
 plt.subplot(2, 2, 1)
 plt.imshow(img, cmap="gray")
-plt.title("Original")
+plt.title("original")
 plt.axis("off")
 
 plt.subplot(2, 2, 2)
 plt.bar(x, hist, width=1.0)
-plt.title("Histograma")
+plt.title("histograma")
 plt.xlim(0, 255)
 plt.grid(True, alpha=0.2)
 
 
 plt.subplot(2, 2, 4)
 plt.imshow(eq, cmap="gray")
-plt.title("Ecualizada")
+plt.title("ecualizado")
 plt.axis("off")
 
 plt.subplot(2, 2, 3)
